42513aad3897f07ffc88edfcc15f4b018f8c3f51
Jim Faulkner <jhf1203@gmail.com>
2020-08-23 22:16:34 -0400

    License & Collaboration sections

--- develop/index.js
+++ develop/index.js
@@ -19,33 +19,38 @@ const writeFileAsync = util.promisify(fs.writeFile);
         },
         {
             type: "input",
-            message: "Got it.  Last question before we talk about your application.  What's your GitHub username?",
+            message: "Got it.  Almost to the meat of your readMe.  What's your GitHub username?",
             name: "promptUserGithub"
         },
         {
             type: "input",
-            message: "Okay!  Now on to the readMe.  First, what would you like to name your file?  You can leave off the extension, I'll take care of that part.",
-            name: "promptFileName"
+            message: "Thanks!  And finally, what's the repo name in which this is housed?  This will help users collaborate with you on future enhancements.",
+            name: "promptUserRepo"
         },
         {
             type: "input",
-            message: "Tell me a little bit about your project.  I'd love to know the what, how, and why behind it.",
+            message: "Okay!  Now on to the readMe.  First, what would you like to name your file?  You can leave off the file extension, I'll take care of that part.",
+            name: "promptFileName"
+        },
+        {
+            type: "editor",
+            message: "Tell me a little bit about your project.  I'd love to know the what, how, and why behind it.  Just type into the text editor that pops up so you can apply line and paragraph breaks if needed.  For a bulleted list just use the '-' symbol with no spaces before.",
             name: "promptFileDescription"
         },
         {
             type: "editor",
-            message: "Great, thanks!  Next we need some installation instructions.  Provide me a step-by-step on how to get your application running.",
+            message: "Great, thanks!  Next we need some installation instructions.  Provide me a step-by-step on how to get your application running.  This will also generate a text editor for you to use.",
             name: "promptFileInstallation"
         },
         {
             type: "editor",
-            message: "Sounds good, let's keep going.  How is your application going to be used?  Please map out a step-by-step walkthrough for your users.",
+            message: "Sounds good, let's keep going.  How is your application going to be used?  Please map out a step-by-step walkthrough for your users.  This will also load in notepad for your convenience.",
             name: "promptFileUsage"
         },
         {
             type: "list",
             message: "Great stuff!  Let's make it official and apply the appropriate licensing to this project",
-            name: "fileUserLicense",
+            name: "promptFileUserLicense",
             choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause License", "BSD 3-Clause License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
         },
     ])
@@ -81,13 +86,26 @@ ${answers.promptFileUsage}
 
 _________________________________
 
+### Collaborating
+
+Got an idea?  A bug to report?  Or even a thought on how the application could run more efficiently?  Log it [here](https://github.com/${answers.promptUserGithub}/${answers.promptUserRepo}/issues) as an issue, and we'll talk about it!
+
+_________________________________
+
 ### Contact the Author
-${answers.promptUserName}
-${answers.promptUserEmail}
-${answers.promptUserGithub}
+
+#### ${answers.promptUserName}
+- [E-mail](${answers.promptUserEmail})
+- [Github](${answers.promptUserGithub})
+
+_________________________________
+
+### License
+
+This application is licensed under the ${answers.promptFileUserLicense}
 `;
 
-    fs.writeFile("myReadMe.md", myReadMe, () => {
+    fs.writeFile(`${answers.promptFileName}-readMe.md`, myReadMe, () => {
         console.log("done!");
     })
 
